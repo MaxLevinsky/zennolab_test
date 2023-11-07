b1d9cbc64626d01991116612fc6807aea8516cc1
MaxLevinsky <70227990+MaxLevinsky@users.noreply.github.com>
2023-11-08 00:55:29 +0300

    Update run_app.py

--- src/zennolab/run_app.py
+++ src/zennolab/run_app.py
@@ -7,7 +7,7 @@ def run_app():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", default="/zennolab/input", help="A path to the input data")
     parser.add_argument("-o", "--output", default="/zennolab/output", help="An output directory")
-
+    parser.add_argument("-d", "--device", default="cpu", help="cpu or cuda")
     args = parser.parse_args()
 
-    calculate_and_save_metric(input_dir=args.input, output_dir=args.output)
+    calculate_and_save_metric(input_dir=args.input, output_dir=args.output, device=args.device)
