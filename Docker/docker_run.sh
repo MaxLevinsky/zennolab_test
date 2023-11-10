1a761fbb794f6a7d6e515d883815b1f94170c9ab
MaxLevinsky <70227990+MaxLevinsky@users.noreply.github.com>
2023-11-10 17:27:19 +0300

    Update docker_run.sh

--- Docker/docker_run.sh
+++ Docker/docker_run.sh
@@ -8,7 +8,7 @@ read -r -p "Output dir: " output_dir
 
 
 docker run --gpus all --rm \
-	--mount type=bind,source=${input_dir},target=/zennolab/input \
+	--mount type=bind,source=${input_dir},target=/zennolab/input,readonly \
 	--mount type=bind,source=${output_dir},target=/zennolab/output \
 	zennolab:latest \
-	run_evaluation -i /zennolab/input -o /zennolab/output -d
+	run_evaluation -i /zennolab/input -o /zennolab/output
