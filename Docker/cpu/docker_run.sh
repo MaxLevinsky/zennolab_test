b4c29d728543fc5a90cb2da974c502dc414fcdbe
Никита Марков <nmarkov@digital-roads.ru>
2023-11-11 23:43:05 +0300

    upd Dockerfile

---
+++ Docker/cpu/docker_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# choose a directory with ground truth json in COCO format
+read -r -p "Input dir: " input_dir
+
+# choose an output dir
+read -r -p "Output dir: " output_dir
+
+
+docker run --rm \
+	--mount type=bind,source=${input_dir},target=/zennolab/input,readonly \
+	--mount type=bind,source=${output_dir},target=/zennolab/output \
+	zennolab:latest \
+	run_evaluation -i /zennolab/input -o /zennolab/output
