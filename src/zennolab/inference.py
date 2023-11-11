2ade318f7db27fe39c837dc0819220283bf715a6
Никита Марков <nmarkov@digital-roads.ru>
2023-11-11 19:53:30 +0300

    rm comments

--- src/zennolab/inference.py
+++ src/zennolab/inference.py
@@ -132,19 +132,3 @@ if __name__ == '__main__':
                               '/home/markov.n/zennolab_test/data/output')
     print(dt.datetime.now() - now)
 
-
-    # from segment_anything import SamPredictor, sam_model_registry
-    #
-    # sam_checkpoint = "sam_vit_h_4b8939.pth"
-    # model_type = "vit_h"
-    # sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-    # sam.to(device='cpu')
-    # predictor = SamPredictor(sam)
-    #
-    # input_box = np.array([0.47409895062446594, 0.36872392892837524, 0.3504016399383545, 0.3696969449520111])
-    # masks, _, _ = predictor.predict(
-    #     point_coords=None,
-    #     point_labels=None,
-    #     box=[0.47409895062446594, 0.36872392892837524, 0.3504016399383545, 0.3696969449520111],
-    #     multimask_output=False,
-    # )
